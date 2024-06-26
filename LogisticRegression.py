fb8bd1abbf663749b596dfdd1ab10806c6415202
fhandrick <handrick@gmail.com>
2024-06-26 23:07:08 +0200

    update dataset avg-min-max (650 features)

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -12,8 +12,8 @@ import csv
 data = pd.read_csv('DS_Network_Flow_Final_Protocol_avg_min_max.csv')
 
 # Select features and target variable
-X = data.drop(['repack','hash'], axis=1)
-y = data['repack']
+X = data.drop(['repack','hash','malicious'], axis=1)
+y = data['malicious']
 z = data['hash']
 z_list = z.values.tolist()
 y_list = y.values.tolist()

8fd3bbd8ae2ba2b6bd93576c27acc6026705c3ac
fhandrick <handrick@gmail.com>
2024-06-26 22:37:11 +0200

    update dataset avg-min-max (650 features)

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,8 +1,9 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 import csv
 
@@ -12,8 +13,8 @@ import csv
 data = pd.read_csv('DS_Network_Flow_Final_Protocol_avg_min_max.csv')
 
 # Select features and target variable
-X = data.drop(['repack','hash'], axis=1)
-y = data['repack']
+X = data.drop(['repack','hash','malicious'], axis=1)
+y = data['malicious']
 z = data['hash']
 z_list = z.values.tolist()
 y_list = y.values.tolist()
@@ -22,8 +23,15 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, random_state=0, test_size=0.3
 )
 
-model = tree.DecisionTreeClassifier()
+#model = tree.DecisionTreeClassifier()
+model = RandomForestClassifier(n_estimators=100, random_state=123, max_depth=5)
 model.fit(X_train, y_train)
+
+fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=800)
+tree.plot_tree(model.estimators_[0],
+               filled = True);
+fig.savefig('rf_individualtree.png')
+
 #predict = model.predict(X).tolist()
 
 #dict = {'Repack': y_list, 'Hash': z_list, 'Pred': predict}  

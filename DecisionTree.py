422f0e73a5944efd4d9932997c12f85028c6b887
fhandrick <handrick@gmail.com>
2024-07-09 00:54:50 +0200

    update at cleaned_file creation

--- DecisionTree.py
+++ DecisionTree.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split,RandomizedSearchCV
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn import tree
 from sklearn.ensemble import RandomForestClassifier
-from scipy.stats import randint
+from sklearn.feature_selection import SelectFromModel
 
 import csv
 
@@ -15,42 +15,14 @@ data = pd.read_csv('cleaned_file.csv')
 # Select features and target variable
 X = data.drop(['hash','malicious'], axis=1)
 y = data['malicious']
-z = data['hash']
-z_list = z.values.tolist()
 y_list = y.values.tolist()
-
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, random_state=0, test_size=0.3
 )
 
-#model = tree.DecisionTreeClassifier()
-#model = RandomForestClassifier(n_estimators=390, random_state=123, max_depth=13)
-model = RandomForestClassifier(n_estimators=270, random_state=123, min_samples_split= 14, min_samples_leaf= 19, max_samples= 3274, max_features= 2000, max_depth=12)
-
-#param_dist = {'n_estimators': randint(50,500),
-#              'max_depth': randint(1,20)}
-#param_dist = {"n_estimators": np.arange(10, 500, 10),
-#           "max_depth": [None, 3, 5, 10, 15],
-#           "min_samples_split": np.arange(2, 20, 2),
-#           "min_samples_leaf": np.arange(1, 20, 2),
-#           "max_features": [1,2000 , "sqrt", "log2"],
-#           "max_samples": [3274]}
-
-#model = RandomForestClassifier(n_jobs=-1, random_state=123)
-
-#rand_search = RandomizedSearchCV(model, 
-#                                 param_distributions = param_dist, 
-#                                 n_iter=5, 
-#                                 cv=5,verbose=True)
-
+model = RandomForestClassifier(n_estimators=330, random_state=123, min_samples_split= 14, min_samples_leaf= 9, max_samples= 3296, max_features= 'log2', max_depth=None)
 model.fit(X_train, y_train)
-#rand_search.fit(X_train, y_train)
-
-#best_rf = rand_search.best_estimator_
-
-#Print the best hyperparameters
-#print('Best hyperparameters:',  rand_search.best_params_)
 
 
 fig, axes = plt.subplots(nrows = 1,ncols = 1,figsize = (4,4), dpi=800)
@@ -59,14 +31,15 @@ tree.plot_tree(model.estimators_[0],
 fig.savefig('rf_individualtree.png')
 
 #predict = model.predict(X).tolist()
-
 #dict = {'Repack': y_list, 'Hash': z_list, 'Pred': predict}  
-       
-# create a Pandas DataFrame from the dictionary
+       # create a Pandas DataFrame from the dictionary
 #df = pd.DataFrame(dict) 
-    
-# write the DataFrame to a CSV file
-#df.to_csv('Neighbors.csv') 
+    # write the DataFrame to a CSV file
+#df.to_csv('randomForest.csv')
 
+predict = model.predict(X_test).tolist()
+print('Total tested: %0d'%len(predict))
+print('Total trained: %0d'%len(y_train))
+print('Total samples: %0d'%len(y_list))
 print(classification_report(y_test, model.predict(X_test)))
 print(confusion_matrix(y_test, model.predict(X_test)))
--- clearFile.py
+++ clearFile.py
@@ -1,8 +1,31 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split,RandomizedSearchCV
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 Malware = pd.read_csv("Final_file.csv")
+
+Malware = Malware.fillna(Malware.iloc[:,5699].median())
+
 unique_columns = Malware.loc[:, Malware.nunique() == 1]
 unique_columns = list(unique_columns.columns)
-Malware_ = Malware.drop(unique_columns, axis=1)
-Malware_cleaned = Malware_.dropna()
+Malware_cleaned = Malware.drop(unique_columns, axis=1)
+
+X = Malware_cleaned.drop(['hash','malicious'], axis=1)
+y = Malware_cleaned['malicious']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, random_state=0, test_size=0.3
+)
+
+model = RandomForestClassifier(n_estimators=230, random_state=123, min_samples_split= 16, min_samples_leaf= 19, max_samples= 3274, max_features= 2000, max_depth=10)
+model.fit(X_train, y_train)
+
+feature_names = model.feature_names_in_
+importances = model.feature_importances_
+feature_imp_df = pd.DataFrame({'Feature': feature_names, 'Gini Importance': importances}).sort_values('Gini Importance', ascending=False) 
+
+Malware_cleaned = Malware_cleaned.loc[:,list(feature_imp_df.Feature[:20])+['hash','malicious']]
+print(Malware_cleaned)
+#Malware_cleaned = Malware_.dropna()
 Malware_cleaned.to_csv("cleaned_file.csv", index = False)
